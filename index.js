234d328ca30179fbc52b7f68dee248ee9987bd84
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-25 16:40:11 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(value){
+    if (value < 42){
+        return (42 - value);
+    }
+        else {
+            return (value -42);
+        }
+    }
+function distanceFromHqInFeet(Value) {
+    return distanceFromHqInBlocks(Value)*264;
+}
+function distanceTravelledInFeet(start,destination){
+    if (destination>start){
+      return((destination-start)*264);
+    }
+    else
+      return((start-destination)*264);
+}
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+      return 0;
+    }
+    if (distance>=400 && distance<=2000){
+      return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+}
